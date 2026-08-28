9c777b64594d0a3a272a06f82bdf42f0f7e52733
2320294 <2320294@fortec.edu.br>
2026-08-28 14:21:02 -0300

    Add files via upload

--- versao.py
+++ versao.py
@@ -1,5 +1,4 @@
-VERSAO_SISTEMA = "Fase 4.8"
-VERSAO_ARQUIVO = "Fase_4_8"
-
-FASE = "5.0"
-VERSAO = "Fase 5.0"
+# Versão central do AutoElétrica
+# ALTERAR SOMENTE AQUI a cada nova fase.
+VERSAO_SISTEMA = "Fase 5.0"
+VERSAO_ARQUIVO = "Fase_5_0"
