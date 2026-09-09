7d4768069fcc55efd6a4481bef22e496ebbe951c
2320294 <2320294@fortec.edu.br>
2026-09-09 13:32:44 -0300

    Add files via upload

--- painel.py
+++ painel.py
@@ -217,7 +217,7 @@ def _garantir_dimensionamento_fisico(
     parametros_projeto
 ):
     """
-    Fase 13.6 Rev.33:
+    Fase 13.6 Rev.34:
     cálculo compartilhado pelas páginas Dimensionamento, Eletrodutos
     e Materiais. O cache também considera método B1/B2 e temperatura.
     """
@@ -683,7 +683,7 @@ def renderizar_painel_principal():
             )
         else:
             st.caption(
-                "Pré-dimensionamento da Fase 13.6 Rev.33. O DG depende da validação "
+                "Pré-dimensionamento da Fase 13.6 Rev.34. O DG depende da validação "
                 "do alimentador e do perfil da concessionária."
             )
 
