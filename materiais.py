26bd59d51b6743fa119afcd53db46158be4a6603
2320294 <2320294@fortec.edu.br>
2026-09-23 11:27:40 -0300

    Add files via upload

--- materiais.py
+++ materiais.py
@@ -3925,7 +3925,7 @@ def renderizar_materiais(
         st.download_button(
             "📊 Exportar para Excel",
             data=excel_bytes,
-            file_name=f"{nome_arquivo}_Circuitos_Materiais_Fase_13_6_Rev_117.xlsx",
+            file_name=f"{nome_arquivo}_ListaMateriais.xls",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             use_container_width=True
         )
@@ -3934,7 +3934,7 @@ def renderizar_materiais(
         st.download_button(
             "📄 Gerar PDF",
             data=pdf_bytes,
-            file_name=f"{nome_arquivo}_Circuitos_Materiais_Fase_13_6_Rev_117.pdf",
+            file_name=f"{nome_arquivo}_ListaMateriais.pdf",
             mime="application/pdf",
             use_container_width=True
         )
